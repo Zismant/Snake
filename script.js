3ae66f8e952eb6170e4bdaafd42f447c2c6cdbe9
Zismant <100025984+Zismant@users.noreply.github.com>
2022-11-05 20:20:01 +0200

    bigSnake

--- script.js
+++ script.js
@@ -6,92 +6,91 @@ document.addEventListener('DOMContentLoaded', () => {
     const el = 15;
     const score = document.querySelector('span');
     score.textContent = 0;
-    
-    
-    
-    const snake = [];
-
-    const snakeHead = {
-        x: el * Math.floor( Math.random() *  (gameWidth / el - 0) + 0 ),
-        y: el * Math.floor( Math.random() *  (gameWidth / el - 0) + 0 ),
-        moveTo: null,
-    };
+    let direction = null;
+    const snake = [{
+        x: el * Math.floor(Math.random() * (gameWidth / el - 0) + 0),
+        y: el * Math.floor(Math.random() * (gameWidth / el - 0) + 0),
+    }];
 
     const food = {};
-    
-    
-    
+
+
+
     function drawFill() {
         ctx.fillStyle = 'aquamarine';
         ctx.fillRect(0, 0, gameWidth, gameHight);
     }
-    
+
     function drawSnake() {
+        snake.forEach((item, i) => {
+            ctx.fillStyle = (i == 0) ? 'red' : 'blue';
+            ctx.fillRect(item.x, item.y, el, el);
+        });
         
-        ctx.fillStyle = 'red';
-        ctx.fillRect(snakeHead.x, snakeHead.y, el, el);
     }
-
+    
     function moveSnake() {
-        switch(snakeHead.moveTo) {
-            case 'left': snakeHead.x -= el;
-            break;
-            case'right': snakeHead.x += el;
-            break;
-            case 'up': snakeHead.y -= el; 
-            break;
-            case 'down': snakeHead.y += el;
-            break;
-        }
-
-        document.addEventListener('keydown', (e) => {
-            if ( (e.code == 'ArrowLeft' || e.code == 'KeyA') && snakeHead.moveTo !== 'right') snakeHead.moveTo = 'left';
-            else if ( (e.code == 'ArrowRight' || e.code == 'KeyD') && snakeHead.moveTo !== 'left') snakeHead.moveTo = 'right';
-            else if ( (e.code == 'ArrowUp' || e.code == 'KeyW') && snakeHead.moveTo !== 'down') snakeHead.moveTo = 'up';
-            else if ( (e.code == 'ArrowDown' || e.code == 'KeyS') && snakeHead.moveTo !== 'up') snakeHead.moveTo = 'down';
-        });   
-
-        if (snakeHead.x < 0) snakeHead.x = gameWidth - el;
-        if (snakeHead.x > gameWidth) snakeHead.x = 0;
-        if (snakeHead.y < 0) snakeHead.y = gameHight - el;
-        if (snakeHead.y > gameHight) snakeHead.y = 0;
+        let snakeX = snake[0].x;
+        let snakeY = snake[0].y;
         
+        document.addEventListener('keydown', (e) => {
+            if ((e.code == 'ArrowLeft' || e.code == 'KeyA') && direction !== 'right') {
+                direction = 'left';
+            } else if ((e.code == 'ArrowRight' || e.code == 'KeyD') && direction !== 'left') {
+                direction = 'right';
+            } else if ((e.code == 'ArrowUp' || e.code == 'KeyW') && direction !== 'down') {
+                direction = 'up';
+            } else if ((e.code == 'ArrowDown' || e.code == 'KeyS') && direction !== 'up') {
+                direction = 'down';
+            }   
+        });
+
+        if (direction == 'left') { snakeX -= el;} 
+        else if (direction == 'right') { snakeX += el;}
+        else if (direction == 'up') { snakeY -= el;} 
+        else if (direction == 'down') { snakeY += el;}
+        
+        
+        if (snake[0].x < 0) { snakeX = gameWidth - el;}
+        if (snake[0].x > gameWidth - el) { snakeX = 0;}
+        if (snake[0].y < 0) {snakeY = gameHight - el;}
+        if (snake[0].y > gameHight - el) {snakeY = 0;}
+        
+        
+        snake.unshift({
+            x: snakeX,
+            y: snakeY,
+        });
+
+        if (snake[0].x == food.x && snake[0].y == food.y) {
+            score.textContent++;
+            createNewFood();  
+        }
+        else {snake.pop();}
     }
 
     function createNewFood() {
-        
-            food.x = el * Math.floor( Math.random() *  (gameWidth / el - 0) + 0 );
-            food.y = el * Math.floor( Math.random() *  (gameHight / el - 0) + 0 );
-        
+
+        food.x = el * Math.floor(Math.random() * (gameWidth / el - 0) + 0);
+        food.y = el * Math.floor(Math.random() * (gameHight / el - 0) + 0);
+
     }
 
     function drawFood() {
         ctx.fillStyle = 'green';
         ctx.fillRect(food.x, food.y, el, el);
     }
-    
-    function snakeEatingFood() {
-        if (snakeHead.x == food.x && snakeHead.y == food.y) {
-            score.textContent++;
-            createNewFood();
-        }
-    }
 
+    
     function game() {
         drawFill();
         drawFood();
-        drawSnake();
         moveSnake();
-        snakeEatingFood();
-        score.value
+        drawSnake();
     }
-    
+
     createNewFood();
     // window.requestAnimationFrame(game);
-    setInterval(game, 300);
+    setInterval(game, 350);
 
 });
-
-    
-
-
